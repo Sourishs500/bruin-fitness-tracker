3feff2b83494eb500cf489a01fa22e4e2715c996
SparshJohri2023 <sparshjohri2004@gmail.com>
2024-02-19 01:15:00 -0800

    Removed the summary processing from the main App.js file

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -6,55 +6,15 @@ import FullHistoryComponents from './components/historyComponents/fullHistoryCom
 import FullSpecificInfoComponents from './components/specificInfoComponents/fullSpecificInfoComponents.js'; 
 
 
-function getPrintableVersion(x) 
-{
-    console.log(x.Workout);
-    return (
-        <p>
-            <b>Workout Details</b> <br/>
-            <table style={{"border":"1px solid black", "borderCollapse":"collapse"}}>
-                <tr>
-                    <th style={{"border":"1px solid black"}}>Exercise</th>
-                    <th style={{"border":"1px solid black"}}>Average Across Sets</th>                
-                </tr>
-            {x.Workout.map(
-                            row => 
-                            {
-                                let message = "";
-                                if (Object.keys(row).length > 0)
-                                {
-                                    message = JSON.stringify(row);
-                                    let ex=row.Exercise;
-                                    let info=row.SetInformation.map(set => {return <span> | {set} | </span>} );
-                                    return (<tr>
-                                                <td style={{"border":"1px solid black"}}>{ex}</td>
-                                                <td style={{"border":"1px solid black"}}>{info}</td>
-                                            </tr>);
-                                }
-                                else return (<tr>
-                                                <td style={{"border":"1px solid black"}}>{"N/A"}</td>
-                                                <td style={{"border":"1px solid black"}}>{"N/A"}</td>
-                                            </tr>);
-                            }
-                          )
-            }
-            </table>
-        </p>
-    )
-}
-
-
 export default function App () {
 
     const dataWithoutGeneralComments = useRef([]);
     const generalNotes = useRef("");
     const completeWorkoutData = useRef({});
-    const [displayFull, updateRecord] = useState("");
     const niwe = "DEFAULT";
     function receiveGeneralNotes({notes})   {generalNotes.current = notes["n"];}
     function receiveData({data})            {dataWithoutGeneralComments.current = data["n"]; 
                                              completeWorkoutData.current = {"GeneralNotes":generalNotes.current, "Workout":dataWithoutGeneralComments.current};
-                                             updateRecord(getPrintableVersion(completeWorkoutData.current) )
                                              console.log("Updated: ", completeWorkoutData.current);
                                             }
 
