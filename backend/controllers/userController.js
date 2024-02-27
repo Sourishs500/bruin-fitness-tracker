f3f93b1765c3bc52422b7cd98cb25473aa004647
Sourishs500 <31145059+Sourishs500@users.noreply.github.com>
2024-02-27 00:58:31 -0800

    added userController, function to add a user to the database

---
+++ backend/controllers/userController.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const User = require('../models/userModel.js')
+
+const createUser = async (req, res) => {
+    const {username, password, gender} = req.body;
+    try{
+        const user = await User.create({username, password, gender});
+        return res.status(200).json(user);
+    }catch(e){
+        return res.status(400).json({error: e.message})
+    }
+}
+
+module.exports = {
+    createUser
+}
