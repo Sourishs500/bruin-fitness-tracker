df703296c9a6d63d20abb144e253014b29388951
SparshJohri2023 <sparshjohri2004@gmail.com>
2024-02-28 01:13:33 -0800

    Making the direction background color less ugly

--- frontend/frontend/src/pages/Home.js
+++ frontend/frontend/src/pages/Home.js
@@ -118,7 +118,7 @@ const Home = () => {
     }
     return (
         <>
-        <div style={{ backgroundColor: 'black' }}><GeneralInformation/></div> 
+        <div style={{ backgroundColor: 'pink' }}><GeneralInformation/></div> 
 
         <div style={{ backgroundColor: 'lightgreen' }}><FullGeneralInfoComponents 
                                                         SendValueUp={receiveGeneralNotes}
