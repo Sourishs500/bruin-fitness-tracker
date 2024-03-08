2dd673fd6e9d23adb38f01d2f3eec8aab6136d41
SparshJohri2023 <sparshjohri2004@gmail.com>
2024-03-08 09:58:37 -0800

    Added to the profile pic so that the number of stars associated with the user is shown (right now hardcoded, will be made to interface with the backend by the next commit)

--- frontend/frontend/src/components/headerComponents/profilePic.js
+++ frontend/frontend/src/components/headerComponents/profilePic.js
@@ -1,10 +1,53 @@
 import kirby from './kirby.png'
+import defaultProfilePic from "./GrayBox.png"
+import { useEffect, useState, useRef } from 'react';
+
+
+async function getGoldStars()
+{
+    return 400; //replace this statement with the implementation of the backend call required to get the actual value
+}
+
+async function getPlatinumStars()
+{
+    return 200; //replace this statement with the implementation of the backend call required to get the actual value
+}
+
 
 export default function ProfilePic()
 {
-    return ( <div> <img className="profilePicture" src={kirby} /> </div>);
+    const [goldStarCount, updateGoldStarCount] = useState(100)
+    const [platinumStarCount, updatePlatStarCount] = useState(100)
 
-    //return (
-    //    <div style={{"display":"inline-block","width":"50px", "height":"50px", "border":"1px solid black"}}> </div>
-    //);
+    const golds = async () => updateGoldStarCount(await getGoldStars());
+    const plats = async () => updatePlatStarCount(await getPlatinumStars());
+    golds();
+    plats();
+    const userLoggedIn = false;
+    //const [userLoggedIn, updateUserLoggedIn] = useState(true);
+    
+    if (userLoggedIn)
+    {
+        return (<span className="topBarRight">
+                    <div style={{display: "flex", flexDirection: "column", alignItems: "end", marginLeft:"25px"}}>
+                        <div style={{marginTop:"5px", fontSize:"20px", fontFamily:"serif"}}>
+                            Gold Stars: {goldStarCount}
+                        </div>
+    
+                        <div style={{marginTop:"5px", fontSize:"20px", fontFamily:"serif"}}>
+                            Platinum Stars: {platinumStarCount}
+                        </div>
+                    </div>
+                    <img className="profilePicture" src={kirby}/>
+                </span>);
+    }
+    else
+    {
+        //
+        //<div style={{border:"1px solid black", height:"70px", width:"70px", marginLeft:"20px", marginRight:"20px"}}></div>
+        return (<span className="topBarRight">
+                    <img className="profilePicture" src={defaultProfilePic}/>
+                </span>
+        );
+    }
 }
