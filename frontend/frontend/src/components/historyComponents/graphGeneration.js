9d45d6f6c7ac2394180484f0fe1ad0e9d2eafc9c
Marvin Mok <marvinmok@Marvins-MacBook-Pro.local>
2024-03-08 01:53:49 -0800

    connected graphs to buttons, todo: route to backend

--- frontend/frontend/src/components/historyComponents/graphGeneration.js
+++ frontend/frontend/src/components/historyComponents/graphGeneration.js
@@ -5,10 +5,11 @@ Remaining Tasks
 2) Call that function so that the graph is displayed inside the first div area
 */
 import React from 'react';
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import { Line } from 'react-chartjs-2'; 
 import {
-  Chart as ChartJS, LineElement, CategoryScale, LinearScale, PointElement, Legend
+  Chart as ChartJS, LineElement, CategoryScale, LinearScale, PointElement, Legend, Filler
 } from 'chart.js';
 
 ChartJS.register( 
@@ -26,22 +27,11 @@ export default function GraphGeneration()
 */
 
 { 
+   
     const allExercises = ["Chest Press", "Preacher Curls", "Wall-Sits", "Bench Press", "Lat Pull-Downs", "Calf Raises"] //hard-coded for now
     const measurements = ["Average", "Maximum", "Minimum"] //this is hard-coded and should stay that way
     const exCount = [...Array(allExercises.length).keys()]
     const measCount=[...Array(measurements.length).keys()]
-    const data = {
-       labels : ["2/25/2024", "2/26/2024", "2/27/2024", "2/28/2024", "2/29/2024", "3/1/2024", "3/2/2024", "3/3/2024", "3/4/2024", "3/5/2024"], 
-     datasets : [{ 
-        label : "The average workout exercises",
-         data: [100,150,200,250,300,350,400,450,500,550],
-         backgroundColor: 'aqua',
-         boderColor: 'black',
-         pointBorderColor: 'aqua',
-         fill: true
-        }
-    ] 
-}
     const options = {
         plugins: {
             legend: true
@@ -50,17 +40,109 @@ export default function GraphGeneration()
             y: {}
         }
     }
+
+    const dates = [
+        ["2/25/2024", "2/26/2024", "2/27/2024", "2/28/2024", "2/29/2024", "3/1/2024", "3/2/2024", "3/3/2024", "3/4/2024", "3/5/2024"],
+        ["3/25/2024", "3/26/2024", "2/27/2024", "2/28/2024", "2/29/2024", "3/1/2024", "3/2/2024", "3/3/2024", "3/4/2024", "3/5/2024"],
+        ["2/25/2024", "4/26/2024", "2/27/2024", "2/28/2024", "2/29/2024", "3/1/2024", "3/2/2024", "3/3/2024", "3/4/2024", "3/5/2024"],
+        ["2/25/2024", "5/26/2024", "2/27/2024", "2/28/2024", "2/29/2024", "3/1/2024", "3/2/2024", "3/3/2024", "3/4/2024", "3/5/2024"],
+        ["2/25/2024", "6/26/2024", "2/27/2024", "2/28/2024", "2/29/2024", "3/1/2024", "3/2/2024", "3/3/2024", "3/4/2024", "3/5/2024"],
+        ["2/25/2024", "7/26/2024", "2/27/2024", "2/28/2024", "2/29/2024", "3/1/2024", "3/2/2024", "3/3/2024", "3/4/2024", "3/5/2024"]
+    ]
+    
+    const datas = 
+    [
+        [
+            [100,150,200,250,300,350,400,450,500,550],
+            [100,150,200,250,300,350,400,450,500,600],
+            [100,150,200,250,300,350,400,450,500,700],
+        ],
+        [
+            [100,150,200,250,300,100,150,200,250,300],
+            [100,150,200,250,300,100,150,200,250,400],
+            [100,150,200,250,300,100,150,200,250,500],
+        ],
+        [
+            [100,150,200,250,300,350,400,450,500,550],
+            [100,150,200,250,300,350,400,450,500,550],
+            [100,150,200,250,300,350,400,450,500,550],
+        ],
+        [
+            [100,150,200,250,300,350,400,450,500,550],
+            [100,150,200,250,300,350,400,450,500,550],
+            [100,150,200,250,300,350,400,450,500,550],
+        ],
+        [
+            [100,150,200,250,300,350,400,450,500,550],
+            [100,150,200,250,300,350,400,450,500,550],
+            [100,150,200,250,300,350,400,450,500,550],
+        ],
+        [
+            [100,150,200,250,300,350,400,450,500,550],
+            [100,150,200,250,300,350,400,450,500,550],
+            [100,150,200,250,300,350,400,450,500,550],
+        ]    
+    ]
+
+    const [exercise, setExercise] = useState(0);
+    const [measurement, setMeasurement] = useState(0);
+    const [data, changeData] = useState({
+        labels : dates[exercise], 
+        datasets : [{ 
+            label : measurements[measurement],
+            data:  datas[exercise][measurement],
+            backgroundColor: 'aqua',
+            boderColor: 'black',
+            pointBorderColor: 'aqua',
+            fill: true
+            }],      
+        });
+    
+    // console.log(exercise);
+    // console.log(measurement);
+    // console.log(datas[exercise][measurement])
+    function ChangeGraph(event) {
+        changeData(
+            {
+            labels : dates[exercise], 
+            datasets : [{ 
+               label : measurements[measurement],
+                data:  datas[exercise][measurement],
+                backgroundColor: 'aqua',
+                boderColor: 'black',
+                pointBorderColor: 'aqua',
+                fill: true
+               }],
+            }
+        )
+    }
     return (
         <div>
             <div style={{ width: '600px', height: '300px', padding: '20px' }}> 
                 <Line data={data} options={options}></Line>
             </div>
             <div>
-                <select key={1} style={{ marginRight: "10px" }}>{exCount.map(category => <option key={category} value="Type of Exercise">{allExercises[category]}</option>)}</select>
-                <select key={2} style={{ marginRight: "10px" }}>{measCount.map(category => <option key={category} value="Type of Measurement">{measurements[category]}</option>)}</select>
-                <Button size="sm" key={3} style={{ marginBottom: "40px", marginTop: "15px" }}>Submit Request</Button>
+                <select 
+                    onChange={e => setExercise(e.target.value)} 
+                    key={1} 
+                    style={{ marginRight: "10px" }}
+                >{
+                    exCount.map(category => <option key={category} value={category}>{allExercises[category]}</option>)
+                }</select>
+                <select 
+                    onChange={e => setMeasurement(e.target.value)} 
+                    key={2} 
+                    style={{ marginRight: "10px" }}
+                >{
+                    measCount.map(category => <option key={category} value={category}>{measurements[category]}</option>)
+                }</select>
+                <Button 
+                    onClick = {ChangeGraph}
+                    size="sm"
+                    key={3} 
+                    style={{ marginBottom: "40px", marginTop: "15px" }}
+                >Submit Request</Button>
             </div>
         </div>
-
     );
 }
