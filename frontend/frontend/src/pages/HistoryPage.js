21a20fc5f15af4211c5edd35bc9a2f56a71f2966
Aryan Gupta <aryaninus@gmail.com>
2024-03-15 19:23:56 -0700

    back to original

--- frontend/frontend/src/pages/HistoryPage.js
+++ frontend/frontend/src/pages/HistoryPage.js
@@ -2,14 +2,10 @@
 import FullHistoryComponents from '../components/historyComponents/fullHistoryComponents.js'; 
 import GetAllMeasures from './Home.js'
 
-<<<<<<< HEAD
-const HistoryPage = ({username, pastDates}) => {
-=======
 const HistoryPage = ({username, pastDates, encrypted}) => {
     //console.log("HELLO!", username)
     //padding: auto
 // , height:500, width:1200, marginLeft: "auto", marginRight: "auto", marginTop:"20px", padding:20, borderRadius:25
->>>>>>> 945d6acb3334732d6def8232708a1ee438e0057a
     return (
         <div style={{ backgroundColor: 'lightblue',}}>
              <div style={{ height:1000, width:1200, marginLeft: "auto", marginRight: "auto", padding:20, borderRadius:25 }}>
